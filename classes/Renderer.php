a504753e8a99f01432b32a30285e4cabd6292eeb
Michal Hozza <mhozza@gmail.com>
2011-04-10 20:43:37 +0000

    Presunute veci z indexu do renderera, pridane templaty

--- classes/Renderer.php
+++ classes/Renderer.php
@@ -1,7 +1,8 @@
 <?php
+require_once 'lib/Twig/Autoloader.php';
 
 require_once 'Gallery.php';
-require_once 'ImageResizer.php';//???
+//require_once 'ImageResizer.php'; TODO: treba to?
 
 
 /**
@@ -10,18 +11,26 @@ require_once 'ImageResizer.php';//???
  */
 class Renderer
 {
-
-  /** Aggregations: */
-
-  /** Compositions: */
-
-   /*** Attributes: ***/  
+  //private $cache = 'templates_cache';
+  private $cache = false;
   
+  private $twig;
+  
+  function __construct() {
+    Twig_Autoloader::register();
+    $loader = new Twig_Loader_Filesystem('templates');  
+    $this->twig = new Twig_Environment($loader, array('cache' => $this->cache));     
+  } 
 
   public function render()
   {
+    $template_path = 'gallery_main.htm';
+    $template = $this->twig->loadTemplate($template_path);
+
     $g = new Gallery();
-    $v['items']= $g->getItems();
+    $vars['gallery']['items']= $g->getItems();    
+        
+    $template->display($vars);
   }
 
 
--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<?php
-  require_once 'lib/Twig/Autoloader.php';
-  require_once 'classes/Renderer.php';
-  Twig_Autoloader::register();
-
-  $cache = 'templates_cache';
-  $cache = false;
-  $loader = new Twig_Loader_Filesystem('templates');  
-  $twig = new Twig_Environment($loader, array('cache' => $cache));
-
-  $template_path = 'gallery_main.htm';
-  $template = $twig->loadTemplate($template_path);
-     
+<?php 
+  require_once 'classes/Renderer.php';     
   $renderer = new Renderer();
-  $vars['gallery'] = $renderer->render();
-        
-  $template->display($vars);
+  $renderer->render();  
 ?>
