f486ef3bd609dcf23a7c6f73225b1a7b49156d2f
Michal Hozza <mhozza@gmail.com>
2012-12-28 20:08:06 +0100

    Premenovanie content na photo_view, dynamicke otvaranie

--- js/photo_view.js
+++ js/photo_view.js
@@ -3,39 +3,56 @@ var imgWidth, imgHeight;
 function resizeWindow()       
 {
   var windowWidth = parseFloat($(window).innerWidth());
-  var mainSpaceLeft = parseFloat($("#content").css("margin-left"))+parseFloat($("#content").css("border-left"));
-  var mainSpaceRight = parseFloat($("#content").css("margin-right"))+parseFloat($("#content").css("border-right"));
+  var mainSpaceLeft = parseFloat($("#photo_view").css("margin-left"))+parseFloat($("#photo_view").css("border-left"));
+  var mainSpaceRight = parseFloat($("#photo_view").css("margin-right"))+parseFloat($("#photo_view").css("border-right"));
   
   var windowHeight = parseFloat($(window).innerHeight());
-  var mainSpaceTop = parseFloat($("#content").css("margin-top"))+parseFloat($("#content").css("border-top"));
-  var mainSpaceBottom = parseFloat($("#content").css("margin-bottom"))+parseFloat($("#content").css("border-bottom"));
+  var mainSpaceTop = parseFloat($("#photo_view").css("margin-top"))+parseFloat($("#photo_view").css("border-top"));
+  var mainSpaceBottom = parseFloat($("#photo_view").css("margin-bottom"))+parseFloat($("#photo_view").css("border-bottom"));
 
   var mainWidth = windowWidth-mainSpaceLeft-mainSpaceRight;
   var mainHeight = windowHeight-mainSpaceTop-mainSpaceBottom;
 
-  $("#content").innerWidth(mainWidth);
-  $("#content").innerHeight(mainHeight);
+  $("#photo_view").innerWidth(mainWidth);
+  $("#photo_view").innerHeight(mainHeight);
 
-  $("#content").css({'left': 0, 'top':0});  
+  $("#photo_view").css({'left': 0, 'top':0});  
   
-  var zoom = Math.max(imgHeight/parseFloat($("#content").height()), imgWidth/parseFloat($("#content").width()));  
+  var zoom = Math.max(imgHeight/parseFloat($("#photo_view").height()), imgWidth/parseFloat($("#photo_view").width()));  
   
-  $("#content img").height(imgHeight/zoom);  
-  $("#content img").width(imgWidth/zoom);  
+  $("#photo_view img").height(imgHeight/zoom);  
+  $("#photo_view img").width(imgWidth/zoom);  
 
-  imgLeft = (parseFloat($("#content").width()) - imgWidth/zoom)/2;
-  imgTop = (parseFloat($("#content").height()) - imgHeight/zoom)/2;
-  $("#content img").css({'margin-left': imgLeft, 'margin-top':imgTop});  
+  imgLeft = (parseFloat($("#photo_view").width()) - imgWidth/zoom)/2;
+  imgTop = (parseFloat($("#photo_view").height()) - imgHeight/zoom)/2;
+  $("#photo_view img").css({'margin-left': imgLeft, 'margin-top':imgTop});  
 }
 
-//get ImgSize
-$("<img/>") // Make in memory copy of image to avoid css issues
-    .attr("src", $("#content img").attr("src"))
+$(window).resize(function(){resizeWindow();});
+
+
+
+// $(document).ready(function(){openWindow();});
+
+function openWindow()
+{
+  html = '\
+  <div id="shadow"></div>\
+  <div id="photo_view">\
+    <img src="gallery/cesta_do_neznama_wallpaper.jpg"/>\
+  </div>\
+  ';
+
+  $("body").append(html);
+
+  //get ImgSize
+  $("<img/>") // Make in memory copy of image to avoid css issues
+    .attr("src", $("#photo_view img").attr("src"))
     .load(function() {
         imgWidth = this.width;   // Note: $(this).width() will not
         imgHeight = this.height; // work for in memory images.
         resizeWindow();
     });
 
-$(document).ready(function(){resizeWindow();})
-$(window).resize(function(){resizeWindow();});
+  resizeWindow();
+}
