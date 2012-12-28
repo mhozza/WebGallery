0703560835273b768efb5e1ec2d279d1beef9a56
Michal Hozza <mhozza@gmail.com>
2012-12-28 21:42:27 +0100

    Pridana podpora zatvarania

--- js/photo_view.js
+++ js/photo_view.js
@@ -2,23 +2,23 @@ var imgWidth, imgHeight;
 
 function resizeWindow()       
 {
-  var windowWidth = parseFloat($(window).innerWidth());
-  var mainSpaceLeft = parseFloat($("#photo_view").css("margin-left"))+parseFloat($("#photo_view").css("border-left"));
-  var mainSpaceRight = parseFloat($("#photo_view").css("margin-right"))+parseFloat($("#photo_view").css("border-right"));
+  windowWidth = parseFloat($(window).innerWidth());
+  mainSpaceLeft = parseFloat($("#photo_view").css("margin-left"))+parseFloat($("#photo_view").css("border-left"));
+  mainSpaceRight = parseFloat($("#photo_view").css("margin-right"))+parseFloat($("#photo_view").css("border-right"));
   
-  var windowHeight = parseFloat($(window).innerHeight());
-  var mainSpaceTop = parseFloat($("#photo_view").css("margin-top"))+parseFloat($("#photo_view").css("border-top"));
-  var mainSpaceBottom = parseFloat($("#photo_view").css("margin-bottom"))+parseFloat($("#photo_view").css("border-bottom"));
+  windowHeight = parseFloat($(window).innerHeight());
+  mainSpaceTop = parseFloat($("#photo_view").css("margin-top"))+parseFloat($("#photo_view").css("border-top"));
+  mainSpaceBottom = parseFloat($("#photo_view").css("margin-bottom"))+parseFloat($("#photo_view").css("border-bottom"));
 
-  var mainWidth = windowWidth-mainSpaceLeft-mainSpaceRight;
-  var mainHeight = windowHeight-mainSpaceTop-mainSpaceBottom;
+  mainWidth = windowWidth-mainSpaceLeft-mainSpaceRight;
+  mainHeight = windowHeight-mainSpaceTop-mainSpaceBottom;
 
   $("#photo_view").innerWidth(mainWidth);
   $("#photo_view").innerHeight(mainHeight);
 
   $("#photo_view").css({'left': 0, 'top':0});  
   
-  var zoom = Math.max(imgHeight/parseFloat($("#photo_view").height()), imgWidth/parseFloat($("#photo_view").width()));  
+  zoom = Math.max(imgHeight/parseFloat($("#photo_view").height()), imgWidth/parseFloat($("#photo_view").width()));  
   
   $("#photo_view img").height(imgHeight/zoom);  
   $("#photo_view img").width(imgWidth/zoom);  
@@ -26,20 +26,25 @@ function resizeWindow()
   imgLeft = (parseFloat($("#photo_view").width()) - imgWidth/zoom)/2;
   imgTop = (parseFloat($("#photo_view").height()) - imgHeight/zoom)/2;
   $("#photo_view img").css({'margin-left': imgLeft, 'margin-top':imgTop});  
+
+  closeW = parseFloat($("#photo_view_close_button").width());
+  closeH = parseFloat($("#photo_view_close_button").height());
+  $("#photo_view_close_button").css({'right':mainSpaceRight-closeW/2, 'top':mainSpaceTop-closeH/2});
 }
 
 $(window).resize(function(){resizeWindow();});
 
-
-
-// $(document).ready(function(){openWindow();});
+$(document).ready(function(){openWindow();});
 
 function openWindow()
 {
   html = '\
+  <div id="apaloosa_gallery_view_wrapper">\
   <div id="shadow"></div>\
   <div id="photo_view">\
     <img src="gallery/cesta_do_neznama_wallpaper.jpg"/>\
+  </div>\
+  <a id="photo_view_close_button" href="javascript:closeWindow()"></a>\
   </div>\
   ';
 
@@ -55,4 +60,9 @@ function openWindow()
     });
 
   resizeWindow();
+}
+
+function closeWindow()
+{
+  $("#apaloosa_gallery_view_wrapper").remove();  
 }
