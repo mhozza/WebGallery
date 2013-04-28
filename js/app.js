1c99242ca8f2b62089c30ad240735f08a2497eb7
Michal Hozza <mhozza@gmail.com>
2013-04-28 21:17:46 +0200

    Jednoduchy angularjs view

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('WebGallery', ['WebGallery.filters', 'WebGallery.services', 'WebGallery.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'templates/js/mainview.html', controller: 'MainViewCtrl'});
+  	// $locationProvider.html5Mode(true);
+    $routeProvider.when('/*albumPath', {templateUrl: 'templates/js/mainview.html', controller: 'MainViewCtrl'})
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,27 @@
 'use strict';
 
 /* Controllers */
-function MainViewCtrl($scope, GalleryItems)
-{	
-	$scope.items = GalleryItems.query();
+function MainViewCtrl($scope, GalleryItems, $routeParams)
+{
+	var path = $routeParams.albumPath;
+	if(path=='')
+		$scope.album = GalleryItems.get(function() {
+  				$scope.setParent($scope.album.parent);
+			});
 
-	$scope.setAlbum = function(path)
-	{
-		$scope.items = GalleryItems.query({path:'"'+path+'"'});		
-	}
+	else
+		$scope.album = GalleryItems.get({path:'"'+path+'"'},function() {
+  				$scope.setParent($scope.album.parent);
+			});
+}
+
+function MainCtrl ($scope) {
+
+ 	$scope.albumParent = null;
+
+ 	$scope.setParent = function(parent)
+ 	{
+ 		$scope.albumParent = parent;
+ 		console.log(parent);
+ 	}
 }
