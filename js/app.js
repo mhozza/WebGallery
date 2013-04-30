b2f33686da5390c86db0ba37295d6d3b63a1004a
Michal Hozza <mhozza@gmail.com>
2013-04-30 22:57:19 +0200

    fix javascript: url

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('WebGallery', ['WebGallery.filters', 'WebGallery.services', 'WebGallery.directives']).
-  config(['$routeProvider', function($routeProvider) {
+  config(function($routeProvider, $locationProvider, $compileProvider) {
   	// $locationProvider.html5Mode(true);
     $routeProvider.when('/*albumPath', {templateUrl: 'templates/js/mainview.html', controller: 'MainViewCtrl'})
     $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+	$compileProvider.urlSanitizationWhitelist(/^\s*(https?|javascript):/);
+  });
